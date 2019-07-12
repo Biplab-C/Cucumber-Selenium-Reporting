56189595aea90c373314149f931ec23dc37f9a88
Biplab Chakraborty <biplabc2@gmail.com>
2019-07-12 10:37:58 +0530

    Biplab is committing

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -8,7 +8,7 @@ public class HomePage {
 	public By Search_Bo= By.xpath("//input[@id='searchText']");
 	public By Submit_Searchbox= By.xpath("//button[@id='searchsubmit']");
 	public By Breadcrumb = By.xpath("//div[@class='breadcrumb clearfix']/a");
-	public By Item_Name = By.xpath("//span[@class='responsivegriditem__title']");
+	public By Item_Name123 = By.xpath("//span[@class='responsivegriditem__title']");
 	public By Item_Name_Link = By.xpath("//span[@class='responsivegriditem__title']/parent::a");
 	public By Item_Name_PDP=By.xpath("//h1[@itemprop='name']");
 	
